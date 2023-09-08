6c991d356dc8c92597cef829f1742bd66edf3a1c
David Gurevich <dmtrgrvch@gmail.com>
2023-09-08 10:07:24 +0300

    homework 3

---
+++ src/main/java/org/example/ExceptionExample.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class ExceptionExample {
+
+    public static void main(String[] args) {
+
+        try {
+            System.out.println(9 / 0);
+
+
+        } catch (Exception e ) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            System.out.println(e.getLocalizedMessage());
+            System.out.println(e.getClass());
+        } finally {
+            System.out.println("finally");
+        }
+
+    }
+}
+
+
+
